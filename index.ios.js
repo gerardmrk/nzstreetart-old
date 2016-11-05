4f4bc15f45f92a394131aa57b617b6e5eb1d8bfc
Gerard Chin <gerardchin91@gmail.com>
2016-11-05 14:52:31 +1300

    Markers work

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,90 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View
-} from 'react-native';
+} from 'react-native'
+import MapView from 'react-native-maps'
+import styles from './styles.index'
 
 export default class NzStreetArt extends Component {
-  render() {
+  state = {
+    position: {
+      latitude: 37.785834,
+      longitude: -122.406417,
+      latitudeDelta: 0.00004508521359580864,
+      longitudeDelta: 0.000044915558749550845
+    },
+    region: {
+      latitude: 37.785834,
+      longitude: -122.406417,
+      latitudeDelta: 0.00004508521359580864,
+      longitudeDelta: 0.000044915558749550845
+    }
+  }
+
+  watchID: ?number = null
+
+  componentDidMount () {
+    window.navigator.geolocation.getCurrentPosition(
+      pos => {
+        const regAndPos = regionFrom(pos)
+        this.setState({ position: regAndPos, region: regAndPos })
+      },
+      err => window.alert(JSON.stringify(err)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    )
+    this['watchID'] = window.navigator.geolocation.watchPosition(pos => {
+      const regAndPos = regionFrom(pos)
+      this.setState({ position: regAndPos, region: regAndPos })
+    })
+  }
+
+  componentWillUnmount () { window.navigator.geolocation.clearWatch(this['watchID']) }
+
+  _handleRegionChange = region => {
+    this.setState({ region, position: region })
+  }
+
+  render () {
+    const { position, region } = this.state
+    const { longitude, latitude } = position
+    console.log(position)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={region}
+          onRegionChange={this._handleRegionChange}
+        >
+          <MapView.Marker
+            coordinate={{ latitude, longitude }}
+            title={'YOU'}
+            description={'Your current location'}
+          />
+        </MapView>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const regionFrom = ({ coords }) => {
+  const { latitude, longitude, accuracy } = coords
 
-AppRegistry.registerComponent('NzStreetArt', () => NzStreetArt);
+  const onDegreeOfLongitudeInMeters = 111.32 * 1000
+  const circumference = (40075 / 360) * 1000
+
+  const latDelta = accuracy * (1 / (Math.cos(latitude) * circumference))
+  const lonDelta = (accuracy / onDegreeOfLongitudeInMeters)
+
+  return {
+    latitude,
+    longitude,
+    latitudeDelta: Math.max(0, latDelta),
+    longitudeDelta: Math.max(0, lonDelta)
+  }
+}
+
+AppRegistry.registerComponent('NzStreetArt', () => NzStreetArt)
